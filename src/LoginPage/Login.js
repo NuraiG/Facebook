71796654ec2fbf9c283508cfde567552cb0b1946
Nuray Gulistan <nuray.b.gulistan@gmail.com>
2021-03-21 16:17:49 +0200

    Added registration modal

--- src/LoginPage/Login.js
+++ src/LoginPage/Login.js
@@ -4,9 +4,21 @@ import {Link} from 'react-router-dom';
 import styles from './Login.module.css';
 import BlueButton from './BlueButton';
 import GreenButton from './GreenButton';
+import Modal from '@material-ui/core/Modal';
+import Registration from './Registration';
 
 
 export default function Login() {
+
+    const [open, setOpen] = React.useState(false);
+  
+    const handleOpen = () => {
+      setOpen(true);
+    };
+  
+    const handleClose = () => {
+      setOpen(false);
+    }
     return (
         <>
             <form className={
@@ -16,15 +28,21 @@ export default function Login() {
                 <div className={
                     styles.input
                 }>
-                    <TextField id="outlined-basic" placeholder="Имейл" variant="outlined"/>
-                    <TextField id="outlined-basic" placeholder="Парола" variant="outlined"/>
+                    <TextField id="outlined-basic" placeholder="Имейл" variant="outlined" required/>
+                    <TextField id="outlined-basic" placeholder="Парола" variant="outlined" required type="password"/>
                 </div>
                 <BlueButton>ВХОД</BlueButton>
                 <Link to='/forgottenPassword'>Забравена парола ?</Link>
                 <Divider flexItem/> {/* import modal for register */}
                 <GreenButton onClick={
-                    () => console.log('green')
+                        handleOpen
                 }>Създаване на нов профил</GreenButton>
+
+                <Modal open={open}
+                    onClose={handleClose}>
+                    <Registration></Registration>
+                </Modal>
+
             </form>
         </>
     )
--- src/LoginPage/Registration.js
+++ src/LoginPage/Registration.js
@@ -1,19 +1,50 @@
-import React from 'react';
-import {TextField, Divider} from '@material-ui/core';
-import styles from './Login.module.css';
+// import React from 'react';
+import React from "react";
+import {TextField} from '@material-ui/core';
 import GreenButton from './GreenButton';
-
+import GenderSelect from './GenderSelect';
+import DatePicker from './DatePicker';
+import styles from './Registration.module.css';
 export default function Registration() {
     return (
-        <div>
-            <h1>Регистрация</h1>
-            <p>Бързо и лесно е.</p>
-            <Divider/>
-            <TextField id="outlined-basic" placeholder="Собствено име" variant="outlined"/>
-            <TextField id="outlined-basic" placeholder="Фамилно име" variant="outlined"/>
-            <TextField id="outlined-basic" placeholder="Имейл" variant="outlined"/>
-            <TextField id="outlined-basic" placeholder="Нова парола" variant="outlined"/>
-            <GreenButton>РЕГИСТРАЦИЯ</GreenButton>
-        </div>
+     
+        <form className={
+            styles.container1
+        }>
+            <div className={
+                styles.header
+            }>
+                <h2>Регистрация</h2>
+                <p>Бързо и лесно е.</p>
+            </div>
+            <div className={
+                styles.container
+            }>
+                <div className={
+                    styles.names
+                }>
+                    <TextField id="outlined-basic" placeholder="Собствено име" variant="outlined" required margin='normal' style={{ alignSelf: 'flex-start' }}/>
+                    <TextField id="outlined-basic" placeholder="Фамилно име" variant="outlined" required margin='normal' style={{ alignSelf: 'flex-end' }}/>
+                </div>
+                <div className={
+                    styles.additional
+                }>
+                    <TextField id="outlined-basic" placeholder="Имейл" variant="outlined" required/>
+                    <TextField id="outlined-basic" placeholder="Нова парола" variant="outlined" required/>
+                    <DatePicker></DatePicker>
+                    <GenderSelect></GenderSelect>
+                </div>
+                <p>
+                    By clicking Регистрация, you agree to our
+                    <a href="https://www.facebook.com/legal/terms/update"> Terms</a>
+                    . Learn how we collect, use and share your data in our
+                    <a href="https://www.facebook.com/about/privacy/update"> Data Policy</a>
+                    and how we use cookies and similar technology in our
+                    <a href="https://www.facebook.com/policies/cookies/"> Cookies Policy</a>
+                    . You may receive SMS Notifications from us and can opt out any time.
+                </p>
+                <GreenButton>РЕГИСТРАЦИЯ</GreenButton>
+            </div>
+        </form>
     )
 }
