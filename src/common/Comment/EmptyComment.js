c32a6c6e513ef073a09ce02ffb2385f270d9eb89
Nuray Gulistan <nuray.b.gulistan@gmail.com>
2021-03-25 22:48:11 +0200

    Added empty comment component

--- src/common/Comment/EmptyComment.js
+++ src/common/Comment/EmptyComment.js
@@ -1,25 +1,68 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PhotoCameraOutlinedIcon from '@material-ui/icons/PhotoCameraOutlined';
 import InsertEmoticonSharpIcon from '@material-ui/icons/InsertEmoticonSharp';
 import Avatar from '@material-ui/core/Avatar';
-import TextField from '@material-ui/core/TextField';
 import styles from './EmptyComment.module.scss';
 
-export default function EmptyComment() {
+export default function EmptyComment(postId, authorName, authorId, authorImage) {
+    const [comment, setComment] = useState("");
+    const addComment = () => {
+        if (comment.length) {
+            setComment("");
+            // add to commenst of a post with id === postId;!!!!
+            // comment{
+            // body:comment,
+            // author:authorId, from id we get can a user image and user name;
+            // likes:0// 0 initial value;
+            // toPost: postId
+            // }
+        }
+    }
+    const addSmileToComment = () => {
+        let add = comment + ' :) ';
+        setComment(add);
+    }
+    const addImageToComment = () => {}
+    // todo;add image to comment
     return (
-        <>
+        <div className={
+            styles.emptyComment
+        }>
             <div className={
-                styles.emptyComment
+                styles.commentAuthor
             }>
-                <Avatar alt="Remy Sharp" src="https://dthezntil550i.cloudfront.net/1f/latest/1f1812170212508330007997070/494e2362-6d3c-40b3-8bf5-192fcea38a2f.png"/>
-                <TextField id="standard-textarea" label="Your comment" placeholder="Write a comment"size="large"/>
-                <InsertEmoticonSharpIcon style={
-                    {fill: "gray"}
-                }/>
-                <PhotoCameraOutlinedIcon style={
-                    {fill: "gray"}
-                }/>
+                <Avatar // alt={authorName}
+                    alt='avatar'
+                    src={authorImage}/>
             </div>
-        </>
+            <form>
+                <input type="text" id="input"
+                    value={comment}
+                    label="Your comment here"
+                    placeholder="Write a comment..."
+                    onChange={
+                        (ev) => setComment(ev.target.value)
+                    }/>
+                <div className={
+                    styles.optional
+                }>
+                    <InsertEmoticonSharpIcon style={
+                            {fill: "gray"}
+                        }
+                        onClick={addSmileToComment}/>
+                    <PhotoCameraOutlinedIcon style={
+                            {fill: "gray"}
+                        }
+                        onClick={addImageToComment}/>
+                </div>
+                <button type="submit"
+                    onClick={
+                        (ev) => {
+                            ev.preventDefault();
+                            addComment()
+                        }
+                }></button>
+        </form>
+    </div>
     )
 }
