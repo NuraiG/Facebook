3645f77843f528bfd1a9e51f1bbc172c20339079
Elitsa Karapchanska <elitsa.karapchanska@gmail.com>
2021-03-25 21:38:53 +0200

    Added example comment structure

--- src/HomePage/Home.js
+++ src/HomePage/Home.js
@@ -5,33 +5,45 @@ import Header from "../Header/Header";
 
 export default function Home() {
   // for testing only
-  let posts = [{
-    postId: 1,
-    author: "id",
-    authorName: "John Doe",
-    authorPhoto: "",
-    postTarget: "id2", // where the post was made
-    postTargetDesc: "wall", // wall/page/group
-    date: "",
-    likes: ["id", "id2"],
-    comments: [""],
-    content: "Wow",
-    timestamp: 1616672141132,
-  },
-  {
-    postId: 2,
-    author: "id",
-    authorName: "John Doe",
-    authorPhoto: "",
-    postTarget: "id", // where the post was made
-    postTargetDesc: "wall", // wall/page/group
-    date: "",
-    likes: ["id", "id2"],
-    comments: [""],
-    content: "Wow",
-    timestamp: 1616672141132,
-  }
-];
+  let posts = [
+    {
+      postId: 1,
+      author: "id",
+      authorName: "John Doe",
+      authorPhoto: "",
+      postTarget: "id2", // where the post was made
+      postTargetDesc: "wall", // wall/page/group
+      likes: ["id", "id2"],
+      comments: [
+        {
+          commentId: 1,
+          author: "id",
+          authorName: "John Doe",
+          authorPhoto: "",
+          likes: [],
+          content: "Wow, a comment",
+          timestamp: 1616672141135,
+          images: [],
+        },
+      ],
+      content: "Wow",
+      images: [],
+      timestamp: 1616672141132,
+    },
+    {
+      postId: 2,
+      author: "id",
+      authorName: "John Doe",
+      authorPhoto: "",
+      postTarget: "id", // where the post was made
+      postTargetDesc: "wall", // wall/page/group
+      likes: ["id", "id2"],
+      comments: [],
+      content: "Wow",
+      images: [],
+      timestamp: 1616672141132,
+    },
+  ];
 
   return (
     <div>
