be15266af3498a72c3fd4c493f0058df87bb4585
Nuray Gulistan <nuray.b.gulistan@gmail.com>
2021-03-25 22:50:13 +0200

    Аdded comment component

---
+++ src/common/Comment/Comment.js
@@ -0,0 +1,54 @@
+import Avatar from '@material-ui/core/Avatar';
+import styles from './Comment.module.scss';
+import {Link} from "react-router-dom";
+import useState from 'react';
+
+export default function Comment(body, authorName, authorImage, likes) {
+    authorName = 'Tom Herzler';
+    likes = ['Tom', 'George', 'Azis'];
+    body = "Waoww gjdshfdshjgdfsdfdgdfhgfhjuyjtyeurytesssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss ";
+    authorImage = 'https://dthezntil550i.cloudfront.net/1f/latest/1f1812170212508330007997070/494e2362-6d3c-40b3-8bf5-192fcea38a2f.png';
+
+    const truncateString = (description, maxLength) => {
+        if (!description) 
+            return null;
+        if (description.length <= maxLength) 
+            return description;
+        return `${
+            description.substring(0, maxLength)
+        }...`;
+    }
+    const addLikes=()=>{
+        // ++this.comment.likes;
+        likes.push('Mari');
+        // likes.push(currentUser.name);
+    }
+    return (
+        <div className={
+            styles.commentWrapper
+        }>
+            <div className={
+                styles.imageWrapper
+            }>
+                <Link to='/login'>
+                    <Avatar alt={authorName}
+                        src={authorImage}/>
+                </Link>
+            </div>
+            <div className={
+                styles.comment
+            }>
+                <div className={
+                    styles.commentBody
+                }>
+                    <p> {authorName}</p>
+                    <div> {
+                        truncateString(body, 100)
+                    }</div>
+                </div>
+                <button onClick={()=>addLikes()}>Like</button>
+                <span>{likes.length}</span>
+            </div>
+        </div>
+    )
+}
