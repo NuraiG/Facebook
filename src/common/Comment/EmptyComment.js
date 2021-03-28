2247ea908c183dfb51dbd25419508c7a3c2284c5
Nuray Gulistan <nuray.b.gulistan@gmail.com>
2021-03-28 20:46:37 +0300

    Refactoring

--- src/common/Comment/EmptyComment.js
+++ src/common/Comment/EmptyComment.js
@@ -1,62 +1,77 @@
-import React, {useState} from 'react';
-import PhotoCameraOutlinedIcon from '@material-ui/icons/PhotoCameraOutlined';
-import InsertEmoticonSharpIcon from '@material-ui/icons/InsertEmoticonSharp';
-import Avatar from '@material-ui/core/Avatar';
-import styles from './EmptyComment.module.scss';
+import React, { useState, useCallback } from "react";
+import PhotoCameraOutlinedIcon from "@material-ui/icons/PhotoCameraOutlined";
+import InsertEmoticonSharpIcon from "@material-ui/icons/InsertEmoticonSharp";
+import Avatar from "@material-ui/core/Avatar";
+import styles from "./EmptyComment.module.scss";
 
-export default function EmptyComment({postId, authorName, authorId, authorImage}) {
-    const [comment, setComment] = useState("");
-    const addComment = () => {
-        if (comment.length) {
-            setComment("");
-            
-        }
+import { useDropzone } from "react-dropzone";
+
+export default function EmptyComment({
+  postId,
+  authorName,
+  authorId,
+  authorImage,
+}) {
+  const [comment, setComment] = useState("");
+  const addComment = () => {
+    if (comment.length) {
+      setComment("");
     }
-    const addSmileToComment = () => {
-        let add = comment + ' :) ';
-        setComment(add);
-    }
-    const addImageToComment = () => {}
-    // todo;add image to comment
-    return (
-        <div className={
-            styles.emptyComment
-        }>
-            <div className={
-                styles.commentAuthor
-            }>
-                <Avatar // alt={authorName}
-                    alt='avatar'
-                    src={authorImage}/>
-            </div>
-            <form>
-                <input type="text" id={postId}
-                    value={comment}
-                    label="Your comment here"
-                    placeholder="Write a comment..."
-                    onChange={
-                        (ev) => setComment(ev.target.value)
-                    }/>
-                <div className={
-                    styles.optional
-                }>
-                    <InsertEmoticonSharpIcon style={
-                            {fill: "gray"}
-                        }
-                        onClick={addSmileToComment}/>
-                    <PhotoCameraOutlinedIcon style={
-                            {fill: "gray"}
-                        }
-                        onClick={addImageToComment}/>
-                </div>
-                <button type="submit"
-                    onClick={
-                        (ev) => {
-                            ev.preventDefault();
-                            addComment()
-                        }
-                }></button>
-        </form>
+  };
+  const addSmileToComment = () => {
+    let add = comment + " :) ";
+    setComment(add);
+  };
+  
+  // todoadd image to comment
+
+  let [attachedFiles, setAttachedFiles] = useState([]);
+  const onDrop = useCallback(
+    (newFiles) => {
+      setAttachedFiles([...attachedFiles, ...newFiles]);
+    },
+    [attachedFiles]
+  );
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop,
+    accept: "image/*",
+  });
+  return (
+    <div className={styles.emptyComment}>
+      <div className={styles.commentAuthor}>
+        <Avatar // alt={authorName}
+          alt="avatar"
+          src={authorImage}
+        />
+      </div>
+      <form>
+        <input
+          type="text"
+          id={postId}
+          value={comment}
+          label="Your comment here"
+          placeholder="Write a comment..."
+          onChange={(ev) => setComment(ev.target.value)}
+        />
+        <div className={styles.optional}>
+          <InsertEmoticonSharpIcon
+            style={{ fill: "gray" }}
+            onClick={addSmileToComment}
+          />
+          <input {...getInputProps()}></input>
+          <PhotoCameraOutlinedIcon
+            {...getRootProps({ className: "dropzone" })}
+            style={{ fill: "gray" }}
+          />
+        </div>
+        <button
+          type="submit"
+          onClick={(ev) => {
+            ev.preventDefault();
+            addComment();
+          }}
+        ></button>
+      </form>
     </div>
-    )
+  );
 }
