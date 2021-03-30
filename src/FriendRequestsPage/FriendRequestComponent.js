b1071389b8b637121b24811dda78e108be6c050f
Elitsa Karapchanska <elitsa.karapchanska@gmail.com>
2021-03-30 22:56:16 +0300

    Added some styles

--- src/FriendRequestsPage/FriendRequestComponent.js
+++ src/FriendRequestsPage/FriendRequestComponent.js
@@ -1,24 +1,45 @@
 import React from "react";
-import { blueGreenTheme, grayButtonTheme } from "../customThemes";
+import { blueGreenTheme, grayButtonTheme, grayTheme } from "../customThemes";
+import styles from "./FriendRequestComponent.module.scss";
 // import { getShortDate } from '../timeUtils';
 
-import { Avatar, Button, ThemeProvider } from "@material-ui/core";
+import { Avatar, Button, Paper, ThemeProvider } from "@material-ui/core";
 
 export default function FriendRequestComponent({ user, friendRequestObj }) {
   // let formattedTimestamp = getShortDate(friendRequestObj.timestamp);
   let formattedTimestamp = "1y";
 
   return (
-    <div>
-      <Avatar src={user.profile_image} />
-      <div>{`${user.firstName} ${user.lastName}`}</div>
-      <span>{formattedTimestamp}</span>
-      <ThemeProvider theme={blueGreenTheme}>
-        <Button color="primary" variant="contained">Confirm</Button>
-      </ThemeProvider>
-      <ThemeProvider theme={grayButtonTheme}>
-        <Button color="primary" variant="contained">Remove</Button>
-      </ThemeProvider>
-    </div>
+    <ThemeProvider theme={grayTheme}>
+      <Paper color="secondary" className={styles.wrapper}>
+        <Avatar src={user.profile_image} className={styles.avatar} />
+        <div className={styles.request_info_container}>
+          <div className={styles.request_info}>
+            <h4>{`${user.firstName} ${user.lastName}`}</h4>
+            <span>{formattedTimestamp}</span>
+          </div>
+          <div className={styles.request_info}>
+            <ThemeProvider theme={blueGreenTheme}>
+              <Button
+                color="primary"
+                variant="contained"
+                className={styles.btn}
+              >
+                Confirm
+              </Button>
+            </ThemeProvider>
+            <ThemeProvider theme={grayButtonTheme}>
+              <Button
+                color="primary"
+                variant="contained"
+                className={styles.btn}
+              >
+                Remove
+              </Button>
+            </ThemeProvider>
+          </div>
+        </div>
+      </Paper>
+    </ThemeProvider>
   );
 }
