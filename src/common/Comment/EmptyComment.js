3c0301abec552928127d99fa948b6971c31a1785
Nuray Gulistan <nuray.b.gulistan@gmail.com>
2021-04-04 11:43:46 +0300

    Fixed a bug with emoji picker and refactoring

--- src/common/Comment/EmptyComment.js
+++ src/common/Comment/EmptyComment.js
@@ -1,142 +1,152 @@
-import React, { useState, useCallback } from "react";
-import { useSelector } from "react-redux";
+import React, {useState, useCallback} from "react";
+import {useSelector} from "react-redux";
 
 // material icons
 import PhotoCameraOutlinedIcon from "@material-ui/icons/PhotoCameraOutlined";
 import InsertEmoticonSharpIcon from "@material-ui/icons/InsertEmoticonSharp";
 
-//material ui
-import { Avatar} from "@material-ui/core";
+// material ui
+import {Avatar} from "@material-ui/core";
 
 import styles from "./EmptyComment.module.scss";
 
-import { useDropzone } from "react-dropzone";
-import { storage } from "../../firebase";
-import { createComment } from "../../service";
+import {useDropzone} from "react-dropzone";
+import {storage} from "../../firebase";
+import {createComment} from "../../service";
 
 import Picker from 'emoji-picker-react';
 
-export default function EmptyComment({ postId, postAuthorId }) {
+export default function EmptyComment({postId, postAuthorId}) {
 
-  const currentUser = useSelector((state) => state.currentUser.currentUser);
+    const currentUser = useSelector((state) => state.currentUser.currentUser);
 
-  const [comment, setComment] = useState("");
-  const [chosenEmoji, setChosenEmoji] = useState(null);
-  const [isEmojiPickerOpen,setEmojiPickerOpen]=useState(false);
+    const [comment, setComment] = useState("");
+    const [chosenEmoji, setChosenEmoji] = useState(null);
+    const [isEmojiPickerOpen, setEmojiPickerOpen] = useState(false);
 
-  const addComment = () => {
-    if (comment.trim().length) {
-      createComment(postId, postAuthorId, {
-        createdById: currentUser.id,
-        createdByFullName: currentUser.firstName + " " + currentUser.lastName,
-        createdByPic: currentUser.profile_image || "",
-        content: comment,
-        attachedImages: attachedFiles,
-      });
-      if (attachedFiles.length > 0) {
-        // TODO: add images to profile
-        setAttachedFiles([]);
-      }
-      setComment("");
-    }
-  };
-  const onEmojiClick = (event, emojiObject) => {
-    let add;
-    setChosenEmoji(emojiObject.emoji);
-    chosenEmoji ? add = comment + " " + chosenEmoji + " " : add = comment;
-    setComment(add);
-  };
-
-  const addSmileToComment = () => {
-    setEmojiPickerOpen(true);
-  };
-
-  // todo add image to comment
-
-  let [attachedFiles, setAttachedFiles] = useState([]);
-  const onDrop = useCallback(
-    (newFiles) => {
-      newFiles.forEach((file) => {
-        const uploadTask = storage
-          .ref()
-          .child("images/" + currentUser.id + Date.now())
-          .put(file);
-
-        uploadTask.on(
-          "state_changed",
-          (snapshot) => {
-            var progress =
-              (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-            console.log("Upload is " + progress + "% done");
-          },
-          (error) => {
-            console.log(error);
-          },
-          () => {
-            uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
-              console.log("File available at", downloadURL);
-              setAttachedFiles([...attachedFiles, downloadURL]);
+    const addComment = () => {
+        if (comment.trim().length) {
+            createComment(postId, postAuthorId, {
+                createdById: currentUser.id,
+                createdByFullName: currentUser.firstName + " " + currentUser.lastName,
+                createdByPic: currentUser.profile_image || "",
+                content: comment,
+                attachedImages: attachedFiles
             });
-          }
-        );
-      });
-    },
-    [attachedFiles, currentUser.id]
-  );
-  const { getRootProps, getInputProps } = useDropzone({
-    onDrop,
-    accept: "image/*",
-  });
+            if (attachedFiles.length > 0) { 
+                setAttachedFiles([]);
+            }
+            setComment("");
+        }
+    };
+    const onEmojiClick = (event, emojiObject) => {
+        let add;
+        setChosenEmoji(emojiObject.emoji);
+        chosenEmoji ? add = comment + " " + chosenEmoji + " " : add = comment;
+        setComment(add);
+    };
+
+    const addSmileToComment = () => {
+        setEmojiPickerOpen(true);
+    };
 
 
-  return (
-    <div className={styles.emptyComment}>
-      <div className={styles.commentAuthor}>
-        <Avatar 
-          alt={currentUser.firstName}
-          src={currentUser.profile_image}
-        />
-      </div>
-      <form>
-        <input
-          type="text"
-          id={postId}
-          value={comment}
-          label="Your comment here"
-          placeholder="Write a comment..."
-          onChange={(ev) => setComment(ev.target.value)}
-          multiple
-          onClick={()=>{setEmojiPickerOpen(false)}}
-        />
-         {attachedFiles.length > 0 &&
-                attachedFiles.map((file) => (
-                  <div key={file} className={styles.attached_images_container}>
-                    <img alt="" src={file} className={styles.attached_images} />
-                  </div>
-                ))}
-        <div className={styles.optional}>
-        
-          <InsertEmoticonSharpIcon
-            style={{ fill: "gray" }}
-            onClick={addSmileToComment}
-            fontSize="large"
-          />
-          <input {...getInputProps()}></input>
-          <PhotoCameraOutlinedIcon
-            {...getRootProps({ className: "dropzone" })}
-            style={{ fill: "gray" }}
-            fontSize="large"
-          />
-          {isEmojiPickerOpen ? <Picker onEmojiClick={onEmojiClick} /> : null }
-        </div>
-        <button
-          type="submit"
-          onClick={(ev) => {
-            ev.preventDefault();
-            addComment();
-          }}
-        ></button>
-      </form>
+    let [attachedFiles, setAttachedFiles] = useState([]);
+    
+    const onDrop = useCallback((newFiles) => {
+        newFiles.forEach((file) => {
+            const uploadTask = storage.ref().child("images/" + currentUser.id + Date.now()).put(file);
+
+            uploadTask.on("state_changed", (snapshot) => {
+                var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                console.log("Upload is " + progress + "% done");
+            }, (error) => {
+                console.log(error);
+            }, () => {
+                uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
+                    console.log("File available at", downloadURL);
+                    setAttachedFiles([
+                        ...attachedFiles,
+                        downloadURL
+                    ]);
+                });
+            });
+        });
+    }, [attachedFiles, currentUser.id]);
+    const {getRootProps, getInputProps} = useDropzone({onDrop, accept: "image/*"});
+
+
+    return (
+        <div className={
+            styles.emptyComment
+        }>
+            <div className={
+                styles.commentAuthor
+            }>
+                <Avatar alt={
+                        currentUser.firstName
+                    }
+                    src={
+                        currentUser.profile_image
+                    }/>
+            </div>
+            <form>
+                <input type="text"
+                    id={postId}
+                    value={comment}
+                    label="Your comment here"
+                    placeholder="Write a comment..."
+                    onChange={
+                        (ev) => setComment(ev.target.value)
+                    }
+                    multiple
+                    onClick={
+                        () => {
+                            setEmojiPickerOpen(false)
+                        }
+                    }/> {
+                attachedFiles.length > 0 && attachedFiles.map((file) => (
+                    <div key={file}
+                        className={
+                            styles.attached_images_container
+                    }>
+                        <img alt=""
+                            src={file}
+                            className={
+                                styles.attached_images
+                            }/>
+                    </div>
+                ))
+            }
+                <div className={
+                    styles.optional
+                }>
+                    {
+                    !isEmojiPickerOpen ? <>
+                        <InsertEmoticonSharpIcon style={
+                                {fill: "gray"}
+                            }
+                            onClick={addSmileToComment}
+                            fontSize="large"/>
+                        <input {...getInputProps()}></input>
+                        <PhotoCameraOutlinedIcon {...getRootProps({ className: "dropzone" })}
+                            style={
+                                {fill: "gray"}
+                            }
+                            fontSize="large"/>
+                    </> : null
+                }
+                    {
+                    isEmojiPickerOpen ? <Picker onEmojiClick={onEmojiClick}/> : null
+                } </div>
+                <button type="submit"
+                    onClick={
+                        (ev) => {
+                            ev.preventDefault();
+                            addComment();
+                        }
+                }></button>
+        </form>
     </div>
-  );
+    );
 }
